639fe8e7866f0582f5c07be98897d2f2d4ac2d7e
Andres Pereira <a07pereira@gmail.com>
2021-12-30 15:03:47 -0500

    tests for About and Nav

--- src/components/About/_tests_/index.test.js
+++ src/components/About/_tests_/index.test.js
@@ -3,13 +3,16 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-describe('About component', () => {
-    // First Test
-    it('renders', () => {
-      render(<About />);
-    });
-  
-    // Second Test
-})
-
 afterEach(cleanup);
+
+describe('About component', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
